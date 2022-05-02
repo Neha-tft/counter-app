217ee03814ca09ca31a5aa7e3460349705ba19ec
Neha-tft <mishra.neha@tftus.com>
2022-05-02 16:54:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+// import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Wrapper from './components/Wrapper';
 
 function App() {
+  // useEffect(() => {
+  //   document.body.style.zoom = "75%";
+  // }, []);
   return (
     <Wrapper></Wrapper>
   );
